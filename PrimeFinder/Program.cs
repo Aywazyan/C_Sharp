450caad8c1ae548c986c709eb6f203d87fbcf091
Aywazyan <ayvazyanerik29@gmail.com>
2025-04-09 19:34:09 +0400

    Prime Finder

---
+++ PrimeFinder/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace PrimeFinder
+{
+    static class PrimeFinder
+    {
+        static void Main()
+        {
+            int i = 1;
+
+            while (i < 1000000)
+            {
+                bool isPrime = true;
+
+                if (i <= 1)
+                {
+                    isPrime = false;
+                }
+                else
+                {
+                    int j = 2;
+                    while (j < i)
+                    {
+                        if (i % j == 0)
+                        {
+                            isPrime = false;
+                            break;
+                        }
+                        j++;
+                    }
+                }
+
+                if (isPrime)
+                {
+                    Console.WriteLine(i);
+                }
+                i++;
+            }
+        }
+    }
+}
