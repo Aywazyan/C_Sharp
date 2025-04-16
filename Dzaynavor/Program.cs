b0545aad45dbb176c7e02ebf10ce503d0de0b019
Aywazyan <ayvazyanerik29@gmail.com>
2025-04-16 22:10:41 +0400

    asd

---
+++ Dzaynavor/Program.cs
@@ -0,0 +1,27 @@
+﻿string dzaynavor = "aoieu";
+
+string text = "Barev";
+string changedText = "";
+
+for (int i = 0; i < text.Length; i++)
+{
+    // bool isDzaynavor = false;
+    // for (int j = 0; j < dzaynavor.Length; j++)
+    // {
+    //     if (text[i] == dzaynavor[j])
+    //     {
+    //         isDzaynavor = true;
+    //         break;
+    //     }
+    // }
+    if (dzaynavor.IndexOf(text[i]) != -1)
+    {
+        changedText += dzaynavor[Random.Shared.Next(0, dzaynavor.Length)];
+    }
+    else
+    {
+        changedText += text[i];
+    }
+}
+
+System.Console.WriteLine(changedText);
---
+++ Polindrome/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter word: ");
+        string text = Console.ReadLine();
+
+        bool isPalindrome = true;
+
+        for (int i = 0; i < text.Length / 2; i++)
+        {
+            if (text[i] != text[text.Length - 1 - i])
+            {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome)
+        {
+            Console.WriteLine("Is Palindrome");
+        }
+        else
+        {
+            Console.WriteLine("Not Palindrome");
+        }
+    }
+}
